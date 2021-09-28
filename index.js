b829b52d3300edef5fd89f8dc36672d9b469eefa
Rajdeep Paul <rajdeeppaulact@gmail.com>
2021-09-28 23:11:20 +0530

    http check added

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ let mongoose = require('mongoose')
 let cors = require('cors')
 const path = require("path")
 let app = express();
-
 require('dotenv').config()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+}
+app.use();
 app.use(cors())
 mongoose.set('useFindAndModify', false);
 
